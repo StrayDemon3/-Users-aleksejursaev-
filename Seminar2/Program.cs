b4055e5ca57563469f8b5d07737b3b867eda6058
Алексей Урсаев <rom-anti-k@yandex.ru>
2022-10-05 14:13:16 +0500

    MultipleOfTwoVariables

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -20,7 +20,7 @@ Console.WriteLine($"The biggest digit of {number} is {biggestDigit}");
 */
 
 // Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
-
+/*
 int RemovingADigitFromAThreeDigitNumber(int num)
 {
     int firstDigit = num / 100;
@@ -28,11 +28,34 @@ int RemovingADigitFromAThreeDigitNumber(int num)
     int result = firstDigit * 10 + secondDigit;
     return result;
 }
-int number = 123;
+int number = new Random().Next(100,1000);
 int res = RemovingADigitFromAThreeDigitNumber(number);
-Console.WriteLine(res);
-
+Console.WriteLine("Removing A Digit From A Three Digit Number " + res);
+*/
 
 // Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно a и b. a и b - инфо от пользователя(тоже int) 
 
+bool MultipleOfTwoVariables(int num, int a, int b)
+{
+    bool result;
+
+    if (num % a == 0 && num % b == 0)
+    {
+        result = true;
+    }
+    else
+    {
+        result = false;
+    }
+
+    return result;
+}
+Console.Write("Enter a divisor ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the first divisor ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second divisor ");
+int b = Convert.ToInt32(Console.ReadLine());
+bool res = MultipleOfTwoVariables(number, a, b);
+Console.WriteLine("Multiple Of Two Variables is" + res);
 
